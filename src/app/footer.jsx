41f1287f841d71ad68fe137331b4352a1184a535
sushilhemrom500530 <sushilhemrom98@gmail.com>
2025-09-24 20:05:40 +0600

    add responsive footer

--- src/app/footer.jsx
+++ src/app/footer.jsx
@@ -3,6 +3,7 @@ import Logo from "./components/logo";
 import logoImage from "@/assets/footer-logo.png";
 import Image from "next/image";
 import appleIcon from "../assets/apple.png";
+import playstore from "../assets/playstore.png";
 import youtube from "../assets/youtube.png";
 import twitter from "../assets/twitter.png";
 import facebook from "../assets/facebook.png";
@@ -10,15 +11,15 @@ import insta from "../assets/instagram.png";
 import Link from "next/link";
 
 export default function Footer() {
-  const StoreCard = () => {
+  const StoreCard = ({ title = "Google Play", src = "" }) => {
     return (
-      <div className="flex items-center gap-2 p-2 border border-[#347C30] rounded cursor-pointer">
+      <div className="flex items-center gap-2 p-2 border border-[#347C30] rounded cursor-pointer w-max">
         <div className="">
-          <Image src={appleIcon} alt="apple-store" className="w-full h-full" />
+          <Image src={src} alt="apple-store" className="w-full h-full" />
         </div>
         <div className="text-white">
           <h4 className="text-[10px]">Download on the</h4>
-          <h2 className="text-[21px] font-medium">App Store</h2>
+          <h2 className="text-[22px] font-medium">{title}</h2>
         </div>
       </div>
     );
@@ -29,26 +30,26 @@ export default function Footer() {
       style={{
         backgroundImage: `url(${coverImage.src})`,
       }}
-      className="bg-[#0F3B34] lg:h-[378px] bg-no-repeat bg-cover bg-center "
+      className="bg-[#0F3B34] h-auto pb-6 lg:h-[378px] bg-no-repeat bg-cover bg-center "
     >
       <div className=" w-full h-full custom-container  ">
-        <div className="flex flex-col md:flex-row items-center justify-between py-[6.5rem] ">
+        <div className="flex flex-col lg:flex-row items-center justify-between gap-y-12 py-[6.4rem] ">
           <div>
             <Logo src={logoImage} />
           </div>
           <div>
-            <p className="lg:w-[404px] text-[#CFD8D6]">
+            <p className="md:w-[404px] text-[#CFD8D6] text-center lg:text-start">
               Your all-in-one platform for job scheduling, employee management,
               and client service built to keep your business running smoothly
               from anywhere.
             </p>
           </div>
-          <div className="flex items-center gap-5 lg:w-[386px]">
-            <StoreCard />
-            <StoreCard />
+          <div className="flex items-center  flex-col md:flex-row gap-5 w-max">
+            <StoreCard title="App Store" src={appleIcon} />
+            <StoreCard title="Google Play" src={playstore} />
           </div>
         </div>
-        <div className="flex items-center gap-6">
+        <div className="flex items-center gap-6 mb-6">
           <Link href="#">
             <Image
               src={youtube}
